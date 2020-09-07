712cc466635c91245889d0f3c97b92be1635f116
joseb37 <guille11@uwindsor.ca>
2020-09-07 00:33:07 -0400

    documented and cleaned up user_updated_graphs.py

--- user_updated_graphs.py
+++ user_updated_graphs.py
@@ -1,22 +1,21 @@
+import datetime
 import dash
-import pandas_datareader
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import datetime
 import pandas_datareader.data as web
-
+from dash.dependencies import Input, Output
 
 app = dash.Dash()
 app.layout = html.Div(children=[
     html.Div(children='''
         symbol to graph:
     '''),
-    dcc.Input(id='input', value='', type='text'),
-    html.Div(id='output-graph')
+    dcc.Input(id='input', value='', type='text'),  # creates a place for input
+    html.Div(id='output-graph')  # creates a place for output to go
 ])
 
-@app.callback(
+
+@app.callback(  # indicates where the input is coming from and where output is going
     Output(component_id='output-graph', component_property='children'),
     [Input(component_id='input', component_property='value')]
 )
@@ -24,20 +23,24 @@ def update_graph(input_data):
     try:
         start = datetime.datetime(2015, 1, 1)
         end = datetime.datetime.now()
-        df = web.DataReader(input_data, 'yahoo', start, end)
+        df = web.DataReader(input_data, 'yahoo', start, end)  # puts data in df
+        df.reset_index(inplace=True)
+        df.set_index("Date", inplace=True)
+        df = df.drop("High", axis='columns')
         return dcc.Graph(
             id='example-graph',
             figure={
                 'data': [
                     {'x': df.index, 'y': df.Close, 'type': 'line', 'name': input_data},
-                ],
+                ],  # graphs time v closing price of stock
                 'layout': {
                     'title': input_data
                 }
             }
         )
-    except :
+    except:
         return
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
